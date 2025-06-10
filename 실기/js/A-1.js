e625e3278af336973f273c4f77b93237a1cf0d11
GUNW-O-O <rhrjsdn12@naver.com>
2025-06-10 20:04:11 +0900

    6.10

--- 실기/js/A-1.js
+++ 실기/js/A-1.js
@@ -1,12 +1,42 @@
 $(function() {
-   const mainMenu = $('.main-menu > .menu-item')
-
-   mainMenu.on('mouseover', function() {
-      $('.sub-menu').stop().slideDown()
-   }),
-   mainMenu.on('mouseout', function() {
-      $('.sub-menu').stop().slideUp()
+   $('nav>ul>li').mouseenter(function(){
+      $('.sub').stop().slideDown()
+   })
+   $('nav>ul>li').mouseleave(function(){
+      $('.sub').stop().slideUp()
    })
 
+
+   let i = 0
+   function slide() {
+      if(i >= 2) {
+         i=0
+      }else {
+         i++
+      }
+      $('.img-slide .slides').animate({top:-300*i},500)
+   }
+   setInterval(slide,2000)
+
+   $('.tab div').click(function(){
+      $('.tab div').removeClass('active')
+      $(this).addClass('active')
+      var i = $(this).index()
+      // $('.on').hide()
+      if(i==0){
+         $('.noticecon').addClass('on')
+         $('.gallcon').removeClass('on')
+      }
+      else if (i==1) {
+         $('.gallcon').addClass('on')
+         $('.noticecon').removeClass('on')
+      }
+   })
+   $('.popup').click(function(){
+      $('.modal').show()
+   })
+   $('.btn').click(function(){
+      $('.modal').hide()
+   })
 
 })
